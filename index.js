3e2d6326e2d0f783f4eaf007cb31ad2382af1599
APPLE <apple@192.168.0.103>
2025-12-02 12:49:35 -0400

    se le agregaron las credenciales

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const axios = require('axios'); // Para hacer peticiones HTTP
 const app = express();
-const CLIENT_KEY = "sbawqncaggf04rtmha";
-const CLIENT_SECRET = "6i3JsHC2Xwp1vWCwtHoJN60k5f5IQSZq";
+const CLIENT_KEY = "aw0gu0r5pw4s8f8z";
+const CLIENT_SECRET = "yjuDDzr59AqlXuA5JjXcMKD85NmpLvN7";
 // Asegúrate de que esta URL esté registrada en tu app de TikTok
 const REDIRECT_URI = "http://localhost:3000/callback";
 // Estado temporal para guardar el Access Token (solo para este ejemplo)
