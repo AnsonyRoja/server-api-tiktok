820cbd47226294a94ba0c699734e94d8402ffc76
APPLE <apple@192.168.0.103>
2025-12-02 19:28:03 -0400

    Finalizado

--- index.js
+++ index.js
@@ -15,16 +15,7 @@ let USER_ACCESS_TOKEN = null;
 let REFRESH_TOKEN = null; // Gu
 
 app.use(cookieParser());
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://tudominio.com"
-    ],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 
