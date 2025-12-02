4f04b4a3b3c4c24647d6ce8382df22609cec19c3
APPLE <apple@192.168.0.103>
2025-12-02 19:51:44 -0400

    No genera token

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/callback', async (req, res) => {
         console.log("♻ Nuevo REFRESH TOKEN:", REFRESH_TOKEN);
 
         if (!USER_ACCESS_TOKEN)
-            return res.status(200).send("No se generó access token");
+            return res.status(200).send(`No se generó access token ${data}`);
 
         res.cookie("access_token", USER_ACCESS_TOKEN, {
             httpOnly: true,
